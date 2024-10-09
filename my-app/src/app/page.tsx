f550d720d2f2716b7609214c8fd33dfba03d44c2
root <root@NathansLaptop.>
2024-10-09 16:44:03 +0800

    update3

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import DontDoDrugs from "@/app/assets/dont do drugs.png";
+import { FlipWords } from "@/components/ui/flip-words";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
+  const words: string[] = ["skibidi", "litty", "sigma"];
+
   return (
+    
     <div>
-      <div className="flex flex-col justify-center h-dvh">
-        <p className="font-bold text-5xl text-center">Drug/Substance Abuse</p>
+      <div className="flex flex-col justify-center h-dvh z-10">
+        <h1 className="font-bold text-5xl text-center">Drugs are not <FlipWords words={words}/></h1>
         <br/>
-        <p className="text-sm text-center max-w-5xl mx-auto p-4">Although Singapore has generally been able to handle drug and substance abuse 
+        <p className="text-sm text-center max-w-5xl mx-auto p-4 z-10">Although Singapore has generally been able to handle drug and substance abuse 
                                           problems, the issue unfortunately still persists, especially in lower income 
                                           families where lack of information and awareness about substance abuse is prevelant. 
                                           This is especially an issue for children, teenagers, and young adults, which 
@@ -17,10 +22,11 @@ export default function Home() {
                                           2023, the number of new abusers rose by 19%. With this pattern of increase, 
                                           it is extremely important that drug/substance abuse awareness is equally 
                                           spread across Singapore, regardless of low, middle, high income status.</p>
-        <div className="flex justify-center">
+        <div className="flex justify-center z-10">
           <Image src={DontDoDrugs} alt="DontDoDrugs" width={500}/>
         </div>
       </div>
+    <BackgroundBeams />
     </div>
   );
 }
--- my-app/src/components/navbar/navigation-menu.tsx
+++ my-app/src/components/navbar/navigation-menu.tsx
@@ -1,36 +1,48 @@
 import Link from "next/link";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "../ui/navigation-menu";
 import { ModeToggle } from "../mode-toggle";
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton
+} from '@clerk/nextjs'
 
 export function NavigationBar() {
     return (
-        <NavigationMenu className="flex flex-row justify-between min-w-full list-none h-16 sticky top-0 p-5">
-            <div className="flex flex-row">
-            <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Home
-                    </NavigationMenuLink>
-                </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <Link href="/quiz" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Quiz
-                    </NavigationMenuLink>
-                </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <Link href="/about-me" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        About Me
-                    </NavigationMenuLink>
-                </Link>
-            </NavigationMenuItem>
-            </div>
-            <div>
-                <ModeToggle/>
-            </div>
-        </NavigationMenu>
+            <NavigationMenu className="flex flex-row justify-between min-w-full list-none h-16 sticky top-0 p-5">
+                <div className="flex flex-row">
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Home
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/quiz" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Quiz
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/about-me" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            About Me
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                </div>
+                <div className="flex gap-4">
+                    <ModeToggle/>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </NavigationMenu>
     )
 }
